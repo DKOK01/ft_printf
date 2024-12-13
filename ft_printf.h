f34abb894774cf40b1860fbf912bffdf5db82d12
DKOK01 <aymansadik2005@gmail.com>
2024-12-13 12:03:24 +0100

    .

--- ft_printf.h
+++ ft_printf.h
@@ -1,40 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aysadeq <aysadeq@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/13 11:47:55 by aysadeq           #+#    #+#             */
-/*   Updated: 2024/12/13 12:02:51 by aysadeq          ###   ########.fr       */
+/*   Created: 2024/12/13 11:47:48 by aysadeq           #+#    #+#             */
+/*   Updated: 2024/12/13 11:47:49 by aysadeq          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+# ifndef FT_PRINTF
+#define FT_PRINTF
 
-int	ft_printf(const char *ftm, ...)
-{
-	va_list		args;
-	size_t		count;
-	int			i;
-	size_t		lenth;
-	va_start(args, ftm);
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
 
-	count = 0;
-	i = 0;
-	lenth = ft_strlin(ftm);
-	while(i < lenth)
-	{
-		if (ftm[i] != '%')
-		{
-			count = write(1,&ftm[i],1);
-			i++;
-		}
-		else
-		{
-			if (++i == 'c')
-				ft_putchar();
-		}
-		i++;
-	}
-}
+int ft_printf(const char *, ...);
+
+
+# endif
